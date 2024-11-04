0d19db50a9f3b95d6a41953af1771912430003f4
Ahmed-Elshitehi <elshitehi1@gmail.com>
2024-11-04 04:05:07 +0200

    Cart & Cart Items class

--- src/Cart.java
+++ src/Cart.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+    private List<CartItem> items;
+
+    public Cart() {
+        items = new ArrayList<CartItem>();
+    }
+
+    public void add(Product product, int quantity) throws Exception {
+        if (quantity > product.getQuantity()) {
+            throw new Exception("there is no enough quantity of " + product.getName());
+        }
+        items.add(new CartItem(product, quantity));
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
 }
