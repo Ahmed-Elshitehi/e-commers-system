0dd30270874e9ab30eef21c738f2aad6109472c1
Ahmed-Elshitehi <elshitehi1@gmail.com>
2024-11-04 05:51:44 +0200

    Done (until Now)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.lang.model.type.NullType;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Product cheese = new Product("Cheese", 20, 10, LocalDate.now().plusDays(50), 250);
+        Product tv = new Product("TV", 1500, 5, null, 5000);
+        Customer customer = new Customer("Ahmed", 5000);
+        try {
+            Cart cart = new Cart();
+            cart.add(cheese, 2);
+            cart.add(tv, 1);
 
+            Checkout checkoutService = new Checkout();
+            checkoutService.checkout(customer, cart);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
